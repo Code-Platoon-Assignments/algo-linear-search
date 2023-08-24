ff4024a02ba06eeca2b355fed517cbaf61acb635
Benjamin Cohen <benjamin@codeplatoon.org>
2023-08-24 15:24:26 -0600

    good solution

--- linearSearch.js
+++ linearSearch.js
@@ -1,9 +1,25 @@
 function linearSearch(searchTerm, arr) {
-  return undefined;
+  for (let index = 0; index < arr.length; index++) {
+    const elem = arr[index];
+
+    if (elem === searchTerm) {
+      return index;
+    }
+  }
 }
 
 function globalLinearSearch(searchTerm, arr) {
-  return [];
+  const matchingIndices = [];
+
+  for (let index = 0; index < arr.length; index++) {
+    const elem = arr[index];
+
+    if (elem === searchTerm) {
+      matchingIndices.push(index);
+    }
+  }
+
+  return matchingIndices;
 }
 
 module.exports = { linearSearch, globalLinearSearch };
